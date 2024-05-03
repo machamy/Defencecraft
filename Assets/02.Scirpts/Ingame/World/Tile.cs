101dd8b7c6d27d8f2af13bc086940ea5a9508eaa
machamy <macham2001@gmail.com>
2024-05-03 21:12:52 +0900

    월드 기본 타일 생성
    대충 맵이 어떻게 만들어지는지 감잡으라고 업데이트.
    이 타일위에 그림을 씌운다 생각하면 됨

--- Assets/02.Scirpts/Ingame/World/Tile.cs
+++ Assets/02.Scirpts/Ingame/World/Tile.cs
@@ -1,11 +1,15 @@
 using _02.Scirpts.Ingame.Entity;
+using UnityEngine;
 
 namespace _02.Scirpts.Ingame
 {
-    public class Tile
+    public class Tile : MonoBehaviour
     {
         private int i, j;
         private bool isConstructable;
+        private float size;
+
+        public bool IsInitialized { private set; get; } = false;
 
         /// <summary>
         /// 건축가능 여부
@@ -13,5 +17,29 @@ namespace _02.Scirpts.Ingame
         public bool IsConstructable => isConstructable || Construct != null;
 
         public AbstractConstruct Construct;
+
+        /// <summary>
+        /// 타일 초기화
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <param name="size"></param>
+        /// <param name="isConstructable"></param>
+        /// <param name="chaneState">해당 값으로 크기와 위치를 변경할지 여부</param>
+        public void Init(int i, int j, float size, bool isConstructable = true, bool chaneState = true)
+        {
+            this.i = i;
+            this.j = j;
+            this.size = size;
+            this.isConstructable = isConstructable;
+            IsInitialized = true;
+            if (chaneState)
+            {
+                transform.localPosition = new Vector3(i * size, 0,j * size);
+                transform.localScale = new Vector3(size,transform.localScale.y,size);
+            }
+        }
+        
+        
     }
 }
--- Assets/02.Scirpts/Ingame/World/World.cs
+++ Assets/02.Scirpts/Ingame/World/World.cs
@@ -1,25 +1,56 @@
+using System;
+using UnityEngine;
+
 namespace _02.Scirpts.Ingame
 {
-    public class World
+    public class World : MonoBehaviour
     {
-        private Tile[,] array;
+        private Tile[,] map;
 
-        private int width, height;
+        [SerializeField]
+        private int width = 10, height = 10;
+
+        [SerializeField] private float tileSize;
+        [SerializeField] private GameObject tileBase;
 
         public int Width => width;
         public int Height => height;
-        
-        public World(int height, int width)
+
+        public float TileSize => tileSize;
+
+        private void Start()
         {
-            this.width = width;
-            this.height = height;
-            
-            array = new Tile[height,width];
+            Initialize();
         }
 
+        /// <summary>
+        /// 해당 좌표의 타일을 가져온다.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns>해당 타일</returns>
         public Tile GetTile(int i, int j)
         {
-            return array[i,j];
+            return map[i,j];
+        }
+
+        /// <summary>
+        /// 월드 초기화
+        /// </summary>
+        private void Initialize()
+        {
+            map = new Tile[width,height];
+            
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Tile tile =  Instantiate(tileBase, this.transform).GetComponent<Tile>();
+                    map[i, j] = tile;
+                    tile.Init(i,j,tileSize);
+                    tile.name = $"Tile({i},{j})[C : {tile.IsConstructable}]";
+                }
+            }
         }
     }
 }

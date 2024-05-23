285f15b8f169fe54bb90d1a629c107c38b47b30b
machamy <macham2001@gmail.com>
2024-05-23 22:41:22 +0900

    도둑 클래스 임시로 모노비헤이비어로

--- Assets/02.Scirpts/Ingame/Entity/Unit/Thief.cs
+++ Assets/02.Scirpts/Ingame/Entity/Unit/Thief.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thief : AbstractEnemy
+public class Thief : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

827fe5722610132b018fbd7508ab5817d6c5a44d
KimHyeonUi <khy010802@naver.com>
2024-05-23 21:54:41 +0900

    UIManager 추가 및 일시정지 시 UI 토글 기능 구현

--- Assets/02.Scirpts/Ingame/GameManager.cs
+++ Assets/02.Scirpts/Ingame/GameManager.cs
@@ -1,5 +1,6 @@
 using _02.Scirpts.Ingame;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -9,12 +10,24 @@ public class GameManager : Singleton<GameManager>
     public bool isPaused;
     public float prevTimeScale;
 
+    public enum GameState{ Start, Pause }
     
+    public UnityEvent OnGamePaused;
+    public UnityEvent OnGameResumed;
+    
+    /// <summary>
+    /// 게임 속도를 조정합니다.
+    /// </summary>
+    /// <param name="gameSpeed">조정할 배속</param>
     public void SetGameSpeed(float gameSpeed)
     {
         Time.timeScale = gameSpeed;
     }
 
+    /// <summary>
+    /// 게임을 정지합니다.
+    /// </summary>
+    /// <param name="pause">true 시 정지</param>
     public void Pause(bool pause)
     {
         if (pause)
@@ -22,11 +35,15 @@ public class GameManager : Singleton<GameManager>
             isPaused = true;
             prevTimeScale = Time.timeScale;
             Time.timeScale = 0f;
+            OnGamePaused.Invoke();
         }
         else
         {
             isPaused = false;
             Time.timeScale = prevTimeScale;
+            OnGameResumed.Invoke();
         }
+        
+        
     }
 }

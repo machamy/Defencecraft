7242e46d6c0e99d4ea8d180abd7c89f4549e771a
KimHyeonUi <khy010802@naver.com>
2024-06-26 11:55:02 +0900

    UI 프리팹 일부 완성

---
+++ Assets/02.Scirpts/Ingame/UI/BaseUI.cs
@@ -0,0 +1,7 @@
+﻿namespace _02.Scirpts.Ingame.UI
+{
+    public class BaseUI
+    {
+        
+    }
+}
---
+++ Assets/02.Scirpts/Ingame/UI/MainMenuUI.cs
@@ -0,0 +1,20 @@
+﻿using _02.Scirpts.Ingame.HUD.Button;
+using UnityEngine;
+
+namespace _02.Scirpts.Ingame.UI
+{
+    public class MainMenuUI : BaseUI
+    {
+
+        [SerializeField] private BaseButton button_Start;
+        [SerializeField] private BaseButton button_Credit;
+        [SerializeField] private BaseButton button_Setting;
+
+        public void onAwake()
+        {
+            
+        }
+
+
+    }
+}

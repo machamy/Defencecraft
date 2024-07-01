4657a11d606d68acd0ec30685d1c9baac8e24bef
J-siwon <tldnjs4210@gmail.com>
2024-07-01 13:32:33 +0900

    Tower 공격 투사체 애니메이션 구현
    남은 일
    1. 건물 UI 추가(클릭 시 업그레이드, 철거)
    2. 건물 업그레이드 중 기능 정지

--- Assets/02.Scirpts/Ingame/Entity/Construct/TowerScript/Bullet.cs
+++ Assets/02.Scirpts/Ingame/Entity/Construct/TowerScript/Bullet.cs
@@ -1,35 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float damage;
+    private int level;
+
+    private bool isgenerated = false;
+
     Transform target;
     Rigidbody rigid;
+    Animator animator;
     Vector3 dir;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
-
 
     private void Update()
     {
-        if (target == null)
-        {
+        if(isgenerated) {
+            if (target == null)
+            {
+                StartCoroutine(DestroyBullet());
+                return;
+            }
+
+            dir = target.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(dir);
+
             transform.Translate(Vector3.forward * 3f * Time.deltaTime);
-            return;
         }
 
-        dir = target.position - transform.position;
-        transform.rotation = Quaternion.LookRotation(dir);
-
-        transform.Translate(Vector3.forward * 3f * Time.deltaTime);
     }
 
-    public void Init(float damage, Vector3 dir, Transform target)
+    public void Init(float damage, Vector3 dir, Transform target, int level)
     {
         //총알 데미지
         this.damage = damage;
@@ -39,11 +48,62 @@ public class Bullet : MonoBehaviour
 
         //목표 지점
         this.target = target;
+
+        this.level = level;
+        animator.SetInteger("Level", level);
+        Debug.Log(level);
+
+        isgenerated = false;
+
+        StartCoroutine(GenerateBullet());
     }
 
+    IEnumerator DestroyBullet()
+    {
+        isgenerated = false;
+        rigid.velocity = Vector3.zero;
+        animator.SetTrigger("Destroy");
+
+        while (!animator.GetCurrentAnimatorStateInfo(0).IsName("Bullet_Level1_Destroy") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Bullet_Level2_Destroy") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Bullet_Level3_Destroy"))
+        {
+            yield return null;
+        }
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        // 애니메이션이 끝날때까지 반복
+        while (stateInfo.normalizedTime < 1.0f)
+        {
+            yield return null;
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        }
+
+        Destroy(gameObject);
+    }
+
+    IEnumerator GenerateBullet()
+    {
+        rigid.velocity = Vector3.zero;
+
+        while (!animator.GetCurrentAnimatorStateInfo(0).IsName("Bullet_Level1_Generate") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Bullet_Level2_Generate") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Bullet_Level3_Generate"))
+        {
+            yield return null;
+        }
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        // 애니메이션이 끝날때까지 반복
+        while (stateInfo.normalizedTime < 1.0f)
+        {
+            yield return null;
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        }
+
+        isgenerated = true;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Enemy"))
-            Destroy(gameObject);
+            StartCoroutine(DestroyBullet());
     }
 }

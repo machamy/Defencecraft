37b7b420b7507faea75ed8352e7269044d4e10cc
machamy <macham2001@gmail.com>
2024-07-01 10:36:31 +0900

    게임씬 카메라 해상도 유지

---
+++ Assets/02.Scirpts/CameraResolution.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 화면 비율 고정.
+/// TODO: 비율이 하드코딩 되어있음 ㅎㅎ;
+/// </summary>
+public class CameraResolution: MonoBehaviour
+{
+    void Start()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleheight = ((float)Screen.width / Screen.height) / ((float)20 / 9); // (가로 / 세로)
+        float scalewidth = 1f / scaleheight;
+        if (scaleheight < 1)
+        {
+            rect.height = scaleheight;
+            rect.y = (1f - scaleheight) / 2f;
+        }
+        else
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+
+        camera.rect = rect;
+    }
+
+    void OnPreCull() => GL.Clear(true, true, Color.black);
+}

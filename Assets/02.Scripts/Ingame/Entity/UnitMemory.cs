553a7570fe31034882b386bce8a6c8bca736414d
machamy <macham2001@gmail.com>
2024-10-06 23:22:35 +0900

    시야2를 위한 기억 관리 클래스

---
+++ Assets/02.Scripts/Ingame/Entity/UnitMemory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.Scirpts.Ingame.Entity
+{
+    [Serializable]
+    public class WorldMemory
+    {
+        public TileMemory[][] world;
+        public int width;
+        public int height;
+        
+        public WorldMemory(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            for (int i = 0; i < 100; i++)
+            {
+                world[i] = new TileMemory[100];
+            }
+        }
+    }
+    
+    [Serializable]
+    public struct TileMemory{
+        public int x;
+        public int y;
+        public bool isObstacle;
+        public bool isConstruct;
+        public int priority;
+    }
+}
